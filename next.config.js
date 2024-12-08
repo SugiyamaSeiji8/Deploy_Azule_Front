522a72bc897ef7993e5b8b812d2818df3449f14e
seiji sugiyama <seiji_sugiyama@risebranding.jp>
2024-12-09 00:26:35 +0900

    Update next.config.js
    Signed-off-by: seiji sugiyama <seiji_sugiyama@risebranding.jp>

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Azure Web Appにデプロイする際の設定
   output: 'standalone',
-  reactStrictMode: true,
-  experimental: {
-    optimizeCss: true,
-  }
-}
+  // 必要に応じてbasePath等の設定を追加
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
